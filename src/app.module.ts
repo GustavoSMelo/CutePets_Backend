ba9e87af072ce15aeccd1a95b945e93994c08a77
GustavoSMelo <gsantos15569@gmail.com>
2022-12-12 19:50:20 -0300

    :sparkles: configModule and image module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule as NestJsConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { ConfigModule } from './modules/config/config.module';
+import { ImagesModule } from './modules/images/images.module';
 
 @Module({
-    imports: [],
+    imports: [
+        NestJsConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '../.env',
+        }),
+        ConfigModule,
+        ImagesModule,
+    ],
     controllers: [AppController],
     providers: [],
 })
-export class AppModule { }
+export class AppModule {}
