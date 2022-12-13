0b3384619b7b6c9f492e974cbae1c74a88b1917f
GustavoSMelo <gsantos15569@gmail.com>
2022-12-13 20:57:55 -0300

    :sparkles: add fastify on project

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import {
+    FastifyAdapter,
+    NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
     dotenv.config();
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter(),
+    );
     await app.listen(3000);
 }
 bootstrap();
